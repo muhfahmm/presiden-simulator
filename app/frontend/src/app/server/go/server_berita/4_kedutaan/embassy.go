5a4c80f879c7879d5b42dff5d9f7857fe389c808
Fahim <fahimfahim0407@gmail.com>
2026-04-25 21:36:21 +0700

    presiden-simulator

--- app/frontend/src/app/server/go/server_berita/4_kedutaan/embassy.go
+++ app/frontend/src/app/server/go/server_berita/4_kedutaan/embassy.go
@@ -15,7 +15,7 @@ func GenerateBilateralNews(dateStr string) {
 		n2 = core.NpcNations[core.Rng.Intn(len(core.NpcNations))]
 	}
 	
-	subj := fmt.Sprintf("Pertemuan Bilateral Rutin %s dan %s", n1, n2)
+	subj := fmt.Sprintf("Pembangunan kedutaan besar antara negara %s dengan negara %s disepakati", n1, n2)
 	content := fmt.Sprintf("Delegasi tingkat menteri dari %s dan %s bertemu di gedung kedutaan besar untuk melakukan tinjauan berkala terhadap hubungan diplomatik. Diskusi dilaporkan berjalan secara konstruktif demi kepentingan kedua negara.", n1, n2)
 	
 	core.AddNewsItemLocked("Kantor Berita Kedutaan", subj, content, "diplomacy", "low", dateStr)
